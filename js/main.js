b01636640ff00cc4c52976cf3258745838f258bc
SaraBarrett <sara.c.mon@gmail.com>
2024-07-26 12:45:57 +0100

    vars update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,34 @@
-alert('ola JS');
+//let myName = 'Sara Margarida Monteiro';
+let myFavNumber = 16;
+let number1 = 6;
+let number2 = 60;
+let number3 = 9;
+let myFirstName = 'Sara';
+let mySurname = 'Monteiro';
+let login = false;
+
+//ex1
+let myName = 'Sara';
+
+
+//alert(myName);
+//alert(myFavNumber);
+
+// alert('olá '+myFirstName+' '+mySurname);
+
+// alert('alert1 com luckynumber inicial: O meu nome  é '+myName + ' e o meu número preferido é ' +myLuckyNumber);
+
+// alert('alert2 com luckynumber actualizado: O meu nome  é '+myName + ' e o meu número preferido é ' +myLuckyNumber);
+
+// se login for válido 
+
+//definimos a variável e atribuímos o valor inicial de 16
+let myLuckyNumber = 16;
+
+//actualizámos o valor da variável myLuckyNumber criada acima para 20
+myLuckyNumber = 20;
+
+
+//actualizámos o valor da variável myLuckyNumber, incrementando o que estava em 1valor
+myLuckyNumber++;
+alert(myLuckyNumber);
