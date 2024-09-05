d382965900758046155a789d060e3f2850314369
SaraBarrett <sara.c.mon@gmail.com>
2024-09-05 10:03:06 +0100

    exercicio

--- js/main.js
+++ js/main.js
@@ -1,34 +1,53 @@
-//let myName = 'Sara Margarida Monteiro';
-let myFavNumber = 16;
-let number1 = 6;
-let number2 = 60;
-let number3 = 9;
-let myFirstName = 'Sara';
-let mySurname = 'Monteiro';
-let login = false;
-
-//ex1
-let myName = 'Sara';
-
-
-//alert(myName);
-//alert(myFavNumber);
-
-// alert('olá '+myFirstName+' '+mySurname);
-
-// alert('alert1 com luckynumber inicial: O meu nome  é '+myName + ' e o meu número preferido é ' +myLuckyNumber);
-
-// alert('alert2 com luckynumber actualizado: O meu nome  é '+myName + ' e o meu número preferido é ' +myLuckyNumber);
-
-// se login for válido 
-
-//definimos a variável e atribuímos o valor inicial de 16
-let myLuckyNumber = 16;
-
-//actualizámos o valor da variável myLuckyNumber criada acima para 20
-myLuckyNumber = 20;
+// //let myName = 'Sara Margarida Monteiro';
+// let myFavNumber = 16;
+// let number1 = 6;
+// let number2 = 60;
+// let number3 = 9;
+// let myFirstName = 'Sara';
+// let mySurname = 'Monteiro';
+// let login = false;
+// let jsIsCool = true;
+// //ex1
+// let myName = 'Sara';
 
 
-//actualizámos o valor da variável myLuckyNumber, incrementando o que estava em 1valor
-myLuckyNumber++;
-alert(myLuckyNumber);
+// //alert(myName);
+// //alert(myFavNumber);
+
+// // alert('olá '+myFirstName+' '+mySurname);
+
+// // alert('alert1 com luckynumber inicial: O meu nome  é '+myName + ' e o meu número preferido é ' +myLuckyNumber);
+
+// // alert('alert2 com luckynumber actualizado: O meu nome  é '+myName + ' e o meu número preferido é ' +myLuckyNumber);
+
+// // se login for válido 
+
+// //definimos a variável e atribuímos o valor inicial de 16
+// let myLuckyNumber = 16;
+
+// //actualizámos o valor da variável myLuckyNumber criada acima para 20
+// myLuckyNumber = 20;
+
+
+// //actualizámos o valor da variável myLuckyNumber, incrementando o que estava em 1valor
+// myLuckyNumber++;
+// alert(myLuckyNumber);
+
+// jsIsCool = false;
+
+// let password = 'saraMonteiro16';
+
+// let nameOfCountry = 'Brazil';
+
+// alert(nameOfCountry.toLowerCase().replace('z', 's'));
+
+//password = o que o user escrever;
+
+//console.log('A sua password tem '+password.length);
+
+const word = 'Skateboard';
+
+let facialHair = word.slice(5,10).replace('o','e');
+
+alert(word);
+alert(facialHair);
